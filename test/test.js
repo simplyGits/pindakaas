f66b9a41006d8c6662f5d4080e171d0e533e7416
Thomas Konings <tkon99@gmail.com>
2015-08-16 11:37:23 +0200

    Update test.js
    updated slushy english :P

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var pindakaas = require("../");
 describe("pindakaas", function () {
 	"use strict";
 
-	it("should expose an function", function () {
+	it("should expose a function", function () {
 		expect(pindakaas).to.be.a("function");
 	});
 
